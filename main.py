4a911cfb6600623dd839c3ee45f6e51b9658b024
Jake Eaton <spud-lord@outlook.com>
2020-03-24 16:14:22 +0000

    New Update
    Error Checking

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ def type2(string):
         time.sleep(0.75)
     sys.stdout.write("\n")
 
-#Intro()
+Intro()
 
 first_room = Room("The old ruins of an ancient Castle")
 first_room.set_description("You recognise the old castle as the place you lost your friend. She fell from the tallest tower. The castle has remained closed ever since. But wasn't it destroyed recently?")
@@ -79,18 +79,18 @@ while dead == False:
 
     print(" ")
 
-##    type("You can't figure out why the people want to see this memory")
-##    time.sleep(2)
-##    type("Despite that, you walk forward toward the castle before being hit by one of the guards.")
-##    time.sleep(2)
-##    type("'We are embracing the future now.' they said.")
-##    time.sleep(2)
-##    type("You are confused. But before you could say anything the guard spoke again 'In order to enter, you must answer the question on the door.'")
-##    time.sleep(2)
-##    type("You were about to protest again but the guard cut you up.")
-##    time.sleep(2)
-##    type("'Answer it otherwise you will not enter!' said the Guard. You sigh and look around. There aren't many people but your friend is inside waiting for you. You walk over to the door and read the question with the guard standing right beside you.\n")
-##    time.sleep(2)
+    type("You can't figure out why the people want to see this memory")
+    time.sleep(2)
+    type("Despite that, you walk forward toward the castle before being hit by one of the guards.")
+    time.sleep(2)
+    type("'We are embracing the future now.' they said.")
+    time.sleep(2)
+    type("You are confused. But before you could say anything the guard spoke again 'In order to enter, you must answer the question on the door.'")
+    time.sleep(2)
+    type("You were about to protest again but the guard cut you up.")
+    time.sleep(2)
+    type("'Answer it otherwise you will not enter!' said the Guard. You sigh and look around. There aren't many people but your friend is inside waiting for you. You walk over to the door and read the question with the guard standing right beside you.\n")
+    time.sleep(2)
     type("What is Hardware and Software?\n")
     time.sleep(2)
     type("a: Hardware is the term used to describe the physical components of the computer whereas software is the non-physical programs that are stored on the computer's hardware\n")
@@ -111,14 +111,14 @@ while dead == False:
 
     elif answer1 == "a":
         type("'Well done.' said the guard. He pushed open the door and you enter the castle ruins")
-##        time.sleep(2)
-##        type("You look around and see your friend on the top of the ruins taking selfies")
-##        time.sleep(2)
-##        type("You sigh and turn to move towards the stairs. But when you arrive at them, you are met with another guard")
-##        time.sleep(2)
-##        type("'Come on what is the question?' you ask. The guard smiled and pointed to the sign they were holding")
-##        time.sleep(2)
-##        type("You look at the sign:\n")
+        time.sleep(2)
+        type("You look around and see your friend on the top of the ruins taking selfies")
+        time.sleep(2)
+        type("You sigh and turn to move towards the stairs. But when you arrive at them, you are met with another guard")
+        time.sleep(2)
+        type("'Come on what is the question?' you ask. The guard smiled and pointed to the sign they were holding")
+        time.sleep(2)
+        type("You look at the sign:\n")
         time.sleep(2)
         type("True or False? Library Files are non-compiled files which run when they are needed by a program")
         time.sleep(2)
@@ -161,7 +161,7 @@ while dead == False:
                 type("'Well done. Go on.' said the guard moving aside")
                 time.sleep(2)
                 type("You walk up and look around. You see your friend and some other items. You remember the castle rules saying whatever you find, you can take. It is going to be taken down soon anyway\n")
-                Call()
+                
                      
 
 ##while dead == True:
