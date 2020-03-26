d4e0781cb04307989be7abae95eba03cf5343668
Jake Eaton <spud-lord@outlook.com>
2020-03-26 23:43:21 +0000

    Create clear.py
    Terminal Clear Code

---
+++ clear.py
@@ -0,0 +1,6 @@
+#Jake Eaton
+#Terminal Clear Code
+
+def Clear():
+    if name =='nt':                                                                 #Defines the clear command for the Terminal
+        _ = system('cls')
