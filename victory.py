31f0f66e47248fc9924708577d05d133e7872bc8
Jake Eaton <spud-lord@outlook.com>
2020-03-30 11:09:01 +0100

    Update victory.py
    Story Update

--- victory.py
+++ victory.py
@@ -9,7 +9,7 @@ def Victory():
     time.sleep(2)
     type("Your arms and legs begin to bleed and you yell out in pain")
     time.sleep(2)
-    type("'We have no further use for you and we cannot risk you revealing this information' the woman said")
+    type("'We have no further use for you and we cannot risk you revealing what has happened here' the woman said")
     time.sleep(2)
     type("'Don't worry. You will forget what happened to you.' the woman walked away still holding the gun and you could do nothing...")
     time.sleep(2)
@@ -26,4 +26,4 @@ def Victory():
     type2("Felt...")
     time.sleep(2)
     type2("Nothing...")
-    exit()
+    dead = True
